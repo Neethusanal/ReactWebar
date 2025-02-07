c8a10eddf7cce8cc4342ddd886afabd39d14dc60
Neethu Sanal Kumar <117423137+Neethusanal@users.noreply.github.com>
2025-02-07 12:53:28 +0300

    videosize

--- src/Components/ARDisplay.jsx
+++ src/Components/ARDisplay.jsx
@@ -88,8 +88,8 @@ const ARDisplay = () => {
         <a-video
           src="#videoElement"
           position="0 0 0"
-          width="1"
-          height="0.552"
+          width="50"
+          height="50"
           rotation="0 0 0"
         ></a-video>
       </a-entity>
